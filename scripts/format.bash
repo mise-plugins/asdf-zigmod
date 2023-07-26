42545bdab7d88bf5ce9c5f5571399486020a3bfb
Kenichi Kamiya <kachick1@gmail.com>
2023-07-26 18:35:50 +0900

    Prefer to depend typos with asdf (#60)
    * `asdf local typos latest`
    
    * Prefer to depend typos with asdf
    
    * Why missing the fmt?
    
    * Also enable globstar

--- scripts/format.bash
+++ scripts/format.bash
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+
+shopt -s globstar
+
 shfmt --language-dialect bash --write \
-	./**/*
+	./**/*.bash
 
 dprint fmt
--- scripts/lint.bash
+++ scripts/lint.bash
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+
+shopt -s globstar
+
 shellcheck --shell=bash --external-sources \
 	bin/* --source-path=lib/ \
-	lib/* \
-	scripts/*
+	lib/*.bash \
+	scripts/*.bash
 
 shfmt --language-dialect bash --diff \
-	./**/*
+	./**/*.bash
 
 dprint check
+
+typos . .github .vscode
